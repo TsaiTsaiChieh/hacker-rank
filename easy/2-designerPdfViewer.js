5ed94ac72156cf6fdf1da1a1d536d123a535ed89
Tsai-Chieh <d0177381@gmail.com>
2023-04-13 16:30:30 +0800

    :sparkles: add ai resolution

--- easy/2-designerPdfViewer.js
+++ easy/2-designerPdfViewer.js
@@ -4,6 +4,16 @@ function designerPdfViewer(h, word) {
   return Math.max(...mapping) * mapping.length
 }
 
+// ai
+function designerPdfViewer1(h, word) {
+  let maxHeight = 0
+  for (let i = 0; i < word.length; i++) {
+    const char = word[i];
+    const charHeight = h[char.charCodeAt(0) - 97]
+    maxHeight = Math.max(maxHeight, charHeight)
+  }
+  return maxHeight * word.length
+}
 const [h1, word1] = [
   [
     1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
@@ -12,3 +22,4 @@ const [h1, word1] = [
   "abc",
 ];
 console.log(designerPdfViewer(h1, word1))
+console.log(designerPdfViewer1(h1, word1))
