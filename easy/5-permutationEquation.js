7d3144806c64996e39405dd2315ac578daae04c2
Tsai-Chieh <d0177381@gmail.com>
2023-04-14 16:33:37 +0800

    :recycle: refactor

--- easy/5-permutationEquation.js
+++ easy/5-permutationEquation.js
@@ -1,15 +1,11 @@
 function permutationEquation(p) {
   // Write your code here
-  let mapping = []
   let ans = []
   for (let i = 1; i <= p.length; i++) {
-    mapping.push(p.indexOf(i) + 1)
-  }
-  for (let i = 0; i < p.length; i++) {
-    ans.push(p.indexOf(mapping[i]) + 1)
+    const firstIdx = p.indexOf(i) + 1
+    ans.push(p.indexOf(firstIdx) + 1)
   }
   return ans
 }
 
-// console.log(permutationEquation([2, 3, 1])) // [2, 3, 1]
 console.log(permutationEquation([4, 3, 5, 1, 2])) // [1, 3, 5, 4, 2]
