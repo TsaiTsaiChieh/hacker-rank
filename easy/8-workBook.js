92b081eb73c9d15ef54c1532af86e2360e503f9d
Tsai-Chieh <d0177381@gmail.com>
2023-04-25 10:40:55 +0800

    :sparkles: finish by iterating over each chapter in the workbook

--- easy/8-workBook.js
+++ easy/8-workBook.js
@@ -3,25 +3,26 @@
 // page number index starts at 1
 // special problem is index === page number
 function workbook(n, k, arr) {
-  let record = {};
+  let specialProblems = 0;
   let page = 1;
-
-  // Write your code here
-  const totalPage = arr.reduce((pre, ele) => pre + Math.ceil(ele / k), 0);
-  const totalProblem = arr.reduce((pre, ele) => pre + ele, 0);
-  for (let page = 1; page <= totalPage; page++) {
-    record[page] = [];
-  }
-  for (let j = 0; j < n; j++) {
-    let problemIdx = 1;
-    let pageIdx = 1;
-    const ele = arr[j];
-    while (problemIdx <= ele && totalProblem > 0) {
-      record[pageIdx].push(problemIdx);
-      problemIdx++;
+  for (let i = 0; i < n; i++) {
+    const problemsPerChapter = arr[i];
+    const pagePerChapter = Math.ceil(problemsPerChapter / k);
+    let firstProblemIdx = 1;
+    let lastProblemIdx = k;
+    for (let j = 1; j <= pagePerChapter; j++) {
+      if (
+        firstProblemIdx <= page &&
+        page <= Math.min(problemsPerChapter, lastProblemIdx)
+      ) {
+        specialProblems++;
+      }
+      page++;
+      firstProblemIdx = lastProblemIdx + 1;
+      lastProblemIdx += k;
     }
   }
-  return record;
+  return specialProblems;
 }
 const [n1, k1, arr1] = [5, 3, [4, 2, 6, 1, 10]]; // 4
 console.log(workbook(n1, k1, arr1));
