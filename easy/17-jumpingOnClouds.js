f159a6448955260313361068d2453d0d9c171aad
Tsai-Chieh <d0177381@gmail.com>
2023-04-26 10:07:44 +0800

    :sparkles: add jumpingOnClouds

---
+++ easy/17-jumpingOnClouds.js
@@ -0,0 +1,16 @@
+// Complete the jumpingOnClouds function below.
+function jumpingOnClouds(c, k) {
+  let energy = 100;
+  let idx = 0;
+  do {
+    idx = (idx + k) % c.length;
+    if (c[idx] === 1) energy -= 3;
+    else if (c[idx] === 0) energy -= 1;
+  } while (idx !== 0);
+  return energy;
+}
+
+const [c1, k1] = [[0, 0, 1, 0, 0, 1, 1, 0], 2];
+console.log(jumpingOnClouds(c1, k1)); // 92
+const [c2, k2] = [[1, 1, 1, 0, 1, 1, 0, 0, 0, 0], 3];
+console.log(jumpingOnClouds(c2, k2));
